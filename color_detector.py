3dbb9ef29ca91b44fbb59543c770d53013e9d10a
cristhianpoveda <cristhianpoveda12@gmail.com>
2022-04-21 10:53:55 -0500

    red color detection in the vertical middle 1/3

--- color_detector.py
+++ color_detector.py
@@ -5,15 +5,21 @@ from time import sleep
 
 with picamera.PiCamera() as camera:
     camera.resolution = (320, 240)
-    splits=os.environ['N_SPLITS']
+    totalPixels=108*120
+    splits=int(os.environ['N_SPLITS'])
     print(splits)
-    rangemin=splits/2
+    rangeMin=int(splits/2)
     while True:
         with picamera.array.PiRGBArray(camera) as output:
             camera.capture(output, 'rgb')
             output = output.array
             print(output.shape)
-            for i in range(rangemin, splits, 1):
-                if output[i][160][0] < 200 and output[i][160][0] > 120:
-                    print("Red color detected")
-            sleep(1)
+            redPixels=0
+            for i in range(rangeMin, splits, 1):
+                for j in range (105, 213, 1):
+                    if output[i][j][0] < 200 and output[i][j][0] > 120:
+                        if output[i][j][1] < 100 and output[i][j][2] < 100:
+                            redPixels=redPixels+1
+            redPercentage=100*redPixels/totalPixels
+            print('Red pixels percentage is: ', redPercentage, ' %')
+            sleep(3)
