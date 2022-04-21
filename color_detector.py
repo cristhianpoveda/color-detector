46fb62baf9e5e4a9306fcf1bc8c21c7ea9287d71
cristhianpoveda <cristhianpoveda12@gmail.com>
2022-04-21 09:47:50 -0500

    first try to detect red color

--- color_detector.py
+++ color_detector.py
@@ -5,12 +5,15 @@ from time import sleep
 
 with picamera.PiCamera() as camera:
     camera.resolution = (320, 240)
-    with picamera.array.PiRGBArray(camera) as output:
-        camera.capture(output, 'rgb')
-        output = output.array
-        print(output.shape)
-        splits=os.environ['N_SPLITS']
-        print(splits)
-        for i in range(splits/2, splits):
-            if output[i][160][0] < 200 and output[i][160][0] > 120:
-                print("Red color detected")
+    splits=os.environ['N_SPLITS']
+    print(splits)
+    rangemin=splits/2
+    while True:
+        with picamera.array.PiRGBArray(camera) as output:
+            camera.capture(output, 'rgb')
+            output = output.array
+            print(output.shape)
+            for i in range(rangemin, splits, 1):
+                if output[i][160][0] < 200 and output[i][160][0] > 120:
+                    print("Red color detected")
+            sleep(1)
